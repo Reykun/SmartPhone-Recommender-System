e5413b71527df2ca84604a02c8959d2b2f15d1cf
Reykun <49585683+Reykun@users.noreply.github.com>
2022-12-29 18:37:18 +0700

    Add files via upload

---
+++ Gaming.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.markdown("# Sistem Rekomendasi Smartphone for Gaming ❄️")
+st.sidebar.markdown("# Gaming ❄️")
---
+++ Social Media.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.markdown("# Sistem Rekomendasi Smartphone for Social Media 🎉")
+st.sidebar.markdown("# Social Media 🎉")
